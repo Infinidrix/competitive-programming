bd4f08d8b8689144cf6bee757d53b90cdfdbf4f6
Infinidrix <biruksolomon11@gmail.com>
2019-12-27 12:01:53 +0300

    Rearranging the documents

---
+++ Day 14/rangeSumBST.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# https://leetcode.com/problems/range-sum-of-bst/
+
+class Solution:
+    def return_sorted(self, root):
+        values = []
+        if root.left:
+            values.extend(self.return_sorted(root.left))
+        values.append(root.val)
+        if root.right:
+            values.extend(self.return_sorted(root.right))
+        return values
+    
+    def bin_search(self, array, item, start = 0, end = None):
+        if not end:
+            end = len(array)
+        mid = int (((start+end)/2 - 0.5) + 1)
+        if array[mid] == item:
+            return mid
+        elif array[mid] < item:
+            return self.bin_search(array, item, mid, end)
+        else:
+            return self.bin_search(array, item, start, mid)
+    
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        array = self.return_sorted(root)
+        indexL = self.bin_search(array, L)
+        indexR = self.bin_search(array, R)
+        summ = 0
+        for i in range(indexL, indexR+1):
+            summ += array[i]
+        return summ
