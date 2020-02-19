9ca7a385cc474ac7acfe1726c0fb1f6663ed82dd
Infinidrix <biruksolomon11@gmail.com>
2020-02-19 13:58:59 +0300

    More questions

--- Camp Day 5/lenLongestFibSubseq.py
+++ Camp Day 5/lenLongestFibSubseq.py
@@ -2,20 +2,24 @@
 
 class Solution:
     def lenLongestFibSubseq(self, A: List[int]) -> int:
+        
         fibs = {}
         max_length = 0
+        
         for k in range(len(A)):
-            i = A[k]
-            if i in fibs:
-                for j in fibs[i]:
-                    if i + j[0] not in fibs:
-                        fibs[i+j[0]] = []
-                    fibs[i+j[0]].append((i, j[1]+1))
+            
+            num = A[k]
+            if num in fibs:
+                for j in fibs[num]:
+                    if num + j[0] not in fibs:
+                        fibs[num + j[0]] = set()
+                    fibs[num + j[0]].add((num, j[1]+1))
                     max_length = max(max_length, j[1] + 1)
+                    
             for p in range(k):
                 j = A[p]
-                if i + j not in fibs:
-                    fibs[i+j] = []
-                fibs[i+j].append((i, 2))
+                if num + j not in fibs:
+                    fibs[num+j] = set()
+                fibs[num+j].add((num, 2))
                      
         return max_length
